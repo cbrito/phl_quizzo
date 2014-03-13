f229b2c51472f2ce07a83cad56b4ca81531631b4
Prelang Builder <builder@prelang.com>
2014-03-13 23:06:38 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhlQuizzo::Application.config.secret_key_base = 'b18a3ec97ead412426152a75796e792b9d5cf3471bda76b2958a754e1ee5743c178b4242977578078e8da17a5d83d24c1c37cfe2024ec317746080d87bd3b238'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhlQuizzo::Application.config.session_store :cookie_store, key: '_phl_quizzo_session'
